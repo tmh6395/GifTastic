f429079013c7746b5c9e11b335973d0bd9d78382
Taylor <UkrainianEagle@q.com>
2019-02-06 16:20:19 -0600

    Updated some files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,35 @@
 
-$("#cat-button").on("click", function () {
+let animalArray = [];
 
-    // let word = $("#animal-input").val();
+$("#test-button-press").on("click", function () {
 
-    let queryURL = "https://api.giphy.com/v1/gifs/random?api_key=zQ8KTlIbznfhDm4LIUw6fKleVMQr1UL&tag=" + word;
+    let word = $("#animal-input").val();
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
 
-      .then(function(response){
+    if (!animalArray.includes(word)) {
+        animalArray.push(word);
+        console.log(animalArray);
+        let newButt = $("<button>");
+        newButt.text(word);
+        $("#button-div").append(newButt);
+    }
 
-        console.log(response);
-        // get source of image through response
 
-        // make new image div
-      })
+    let queryURL = "https://api.giphy.com/v1/gifs/random?api_key=zQ8KTlIbznfhDm4LIUw6fKleVMQr1UL8&tag=" + word;
 
-    let newButt = $("<button>");
-    // get query stuff here
-    // give button a name and source
-    $("#button-div").append(newButt)
+
+    function searchAnimalGifs() {
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+
+            .then(function (response) {
+
+                console.log(response);
+            })
+
+
+    }
+
 });
-
-// TO-DO: make separate click handlers for both the "add animal" button and the "search for animal gif" buttons
