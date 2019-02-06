0b8862310479854e03c13ae3068bd8f65bfc2efc
Taylor <UkrainianEagle@q.com>
2019-02-05 22:34:25 -0600

    Updated some files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,27 @@
 
-$("#test-button-press").on("click", function () {
-    console.log("hello");
+$("#cat-button").on("click", function () {
+
+    // let word = $("#animal-input").val();
+
+    let queryURL = "https://api.giphy.com/v1/gifs/random?api_key=zQ8KTlIbznfhDm4LIUw6fKleVMQr1UL&tag=" + word;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+
+      .then(function(response){
+
+        console.log(response);
+        // get source of image through response
+
+        // make new image div
+      })
+
+    let newButt = $("<button>");
+    // get query stuff here
+    // give button a name and source
+    $("#button-div").append(newButt)
 });
+
+// TO-DO: make separate click handlers for both the "add animal" button and the "search for animal gif" buttons
