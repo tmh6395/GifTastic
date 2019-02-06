56ab02ef81f15f790048f128c319de6f1454981c
Taylor <UkrainianEagle@q.com>
2019-02-06 17:25:36 -0600

    Updated some files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,35 +1,51 @@
 
 let animalArray = [];
 
-$("#test-button-press").on("click", function () {
+$("#submit-button").on("click", function () {
 
     let word = $("#animal-input").val();
-
+    let queryURL = "https://api.giphy.com/v1/gifs/search?api_key=zQ8KTlIbznfhDm4LIUw6fKleVMQr1UL8&q=" + word + "&limit=10&lang=en";
 
     if (!animalArray.includes(word)) {
+        // empty the gif-div
+
+        $("#gif-div").empty();
+
+
+
         animalArray.push(word);
         console.log(animalArray);
-        let newButt = $("<button>");
-        newButt.text(word);
-        $("#button-div").append(newButt);
-    }
+        let newButton = $("<button>");
+        newButton.text(word);
+        $("#button-div").append(newButton);
 
 
-    let queryURL = "https://api.giphy.com/v1/gifs/random?api_key=zQ8KTlIbznfhDm4LIUw6fKleVMQr1UL8&tag=" + word;
+        searchAnimalGifs();
 
 
-    function searchAnimalGifs() {
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
+        function searchAnimalGifs() {
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (response) {
+                
+                let results = response.data;
 
-            .then(function (response) {
-
-                console.log(response);
+                for (let i in results) {
+                    // create new image div and put it in a variable
+                    let newGif = $("<img>");
+                    //give it attribute of src of below url
+                    newGif.attr("src", results[i].images.fixed_height.url);
+                    //prepend it to gif-div
+                    $("#gif-div").prepend(newGif);
+                    // console.log(results[i].images.fixed_height.url);
+                }
             })
-
-
+        }
     }
-
 });
+
+
+
+
+
